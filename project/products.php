b02f2e3d6d37a0a2a042887b7d7ef7d8a954ac9d
TranDucNam17 <superouvorlurd@gmail.com>
2024-11-27 11:41:57 +0700

    git add project

---
+++ project/products.php
@@ -0,0 +1,9 @@
+<?php 
+    $products = [
+        ['name' => 'Sản phẩm 1', 'price' => '100000', 'image' => ''],
+        ['name' => 'Sản phẩm 2', 'price' => '200000', 'image' => ''],
+        ['name' => 'Sản phẩm 3', 'price' => '300000', 'image' => ''],
+        ['name' => 'Sản phẩm 4', 'price' => '400000', 'image' => ''],
+        ['name' => 'Sản phẩm 5', 'price' => '500000', 'image' => '']
+    ];
+?>
